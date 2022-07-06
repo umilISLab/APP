a00e0ddc223e47f5340581a59fa4755781791658
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2022-07-06 18:53:45 +0200

    Update iris.sh

--- iris.sh
+++ iris.sh
@@ -3,7 +3,7 @@
 python3 sampling.py --n_iter 100 --n_step 5 --start_size 100 --min_size_global 10 --dataset iris --output samples/iris/s100_mg10.dill
 python3 evaluation.py --iap ap --dataset iris --input samples/iris/s100_mg10.dill --output stats/iris/ap_s100_mg10.dill & 
 python3 evaluation.py --iap iapna --dataset iris --input samples/iris/s100_mg10.dill --output stats/iris/iapna_s100_mg10.dill &
-python3 evaluation.py --iap app --dataset iris --input samples/iris/s100_mg10.dill --output stats/iris/app_s100_mg10.dill --aging_index 1 &
+python3 evaluation.py --iap app --dataset iris --input samples/iris/s100_mg10.dill --output stats/iris/app_s100_mg10.dill --aging_index 1 --pack0 centroid &
 ###########################
 #evolutionary setup
 python3 sampling.py --n_iter 100 --n_step 5 --start_size 5 --min_size_local 5 --min_size_global 10 --dataset iris --output samples/iris/s5_ml5_mg10.dill
